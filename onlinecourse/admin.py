fe6c53638b9812077e8a7c719d05f1a8c31c14de
whatsmynameidontknow <akunhantusaya@gmail.com>
2023-06-07 21:00:43 +0700

    add new models to adminsite

--- onlinecourse/admin.py
+++ onlinecourse/admin.py
@@ -1,16 +1,22 @@
 from django.contrib import admin
-# <HINT> Import any new Models here
-from .models import Course, Lesson, Instructor, Learner
+from .models import Course, Lesson, Instructor, Learner, Enrollment, Question, Choice, Submission
 
-# <HINT> Register QuestionInline and ChoiceInline classes here
+class QuestionInline(admin.StackedInline):
+    model = Question
+    extra = 3
 
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 4
 
 class LessonInline(admin.StackedInline):
     model = Lesson
     extra = 5
 
 
-# Register your models here.
+class QuestionAdmin(admin.ModelAdmin):
+    inlines = [ChoiceInline]
+
 class CourseAdmin(admin.ModelAdmin):
     inlines = [LessonInline]
     list_display = ('name', 'pub_date')
@@ -22,9 +28,10 @@ class LessonAdmin(admin.ModelAdmin):
     list_display = ['title']
 
 
-# <HINT> Register Question and Choice models here
 
 admin.site.register(Course, CourseAdmin)
 admin.site.register(Lesson, LessonAdmin)
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Instructor)
 admin.site.register(Learner)
+admin.site.register(Choice)
